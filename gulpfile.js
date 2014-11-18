a62e5ab356ae68171be3fa31bef3b49908d1bf90
Montana Flynn <montana@montanaflynn.me>
2014-11-18 10:45:52 -0800

    Add mocha task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,17 @@ var gulp = require('gulp')
 var jshint = require('gulp-jshint')
 var uglify = require('gulp-uglify')
 var rename = require('gulp-rename')
+var mocha = require('gulp-mocha')
 
 gulp.task('lint', function() {
     return gulp.src('humandate.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
+})
+
+gulp.task('test', function () {
+    return gulp.src('test.js', {read: false})
+        .pipe(mocha({reporter: 'nyan'}))
 })
 
 gulp.task('build', function() {
@@ -17,7 +23,8 @@ gulp.task('build', function() {
 })
 
 gulp.task('watch', function() {
-    gulp.watch('index.js', ['lint', 'build'])
+    gulp.watch('humandate.js', ['lint', 'test', 'build'])
+    gulp.watch('test.js', ['test'])
 })
 
-gulp.task('default', ['lint', 'build', 'watch'])
+gulp.task('default', ['lint', 'test', 'build', 'watch'])

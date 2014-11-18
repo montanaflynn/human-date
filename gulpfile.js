99f4b47a397b64d387faa864d6ca42423a1598ba
Montana Flynn <montana@montanaflynn.me>
2014-11-18 08:39:44 -0800

    simplify humandate.min.js generation

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,23 @@
-// Include gulp
 var gulp = require('gulp')
-
-// Include Our Plugins
 var jshint = require('gulp-jshint')
 var uglify = require('gulp-uglify')
 var rename = require('gulp-rename')
 
-// Lint Task
 gulp.task('lint', function() {
-    return gulp.src('index.js')
+    return gulp.src('humandate.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
 })
 
-// Uglify JS
 gulp.task('build', function() {
-    return gulp.src('index.js')
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('humandate.min.js'))
+    return gulp.src('humandate.js')
         .pipe(uglify())
-        .pipe(gulp.dest('dist'))
+        .pipe(rename('humandate.min.js'))
+        .pipe(gulp.dest('./'))
 })
 
-// Watch Files For Changes
 gulp.task('watch', function() {
     gulp.watch('index.js', ['lint', 'build'])
 })
 
-// Default Task
 gulp.task('default', ['lint', 'build', 'watch'])
